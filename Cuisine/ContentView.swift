16c9191eece8254e4c22880c2a93e47d03717af9
Nolan Fuchs <Nolan1235@gmail.com>
2024-06-04 10:04:02 -0700

    Initial Commit

---
+++ Cuisine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cuisine
+//
+//  Created by Nolan Fuchs on 6/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cuisine/CuisineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuisineApp.swift
+//  Cuisine
+//
+//  Created by Nolan Fuchs on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CuisineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
